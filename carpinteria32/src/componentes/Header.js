49ca0e5707335c0bb4b9394af3ece9dbc76485e4
JuanJRuiz <juaruiz@andreani.com>
2022-06-27 16:51:16 -0300

    Cambios

---
+++ carpinteria32/src/componentes/Header.js
@@ -0,0 +1,19 @@
+import './confortland.css'
+
+export default function Header() {
+
+    return (
+        <div>
+            <nav>
+                <div class="holder">
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="nosotros.html">¿Quienes Somos?</a></li>
+                        <li><a href="servicios.html">Servicios</a></li>
+                        <li><a href="Galeria.html">Galería</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
